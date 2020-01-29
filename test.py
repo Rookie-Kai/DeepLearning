59e413e1cc388a97aba34d5c9b15e3595a404aee
wangzk <wangzhenkai31@gmail.com>
2020-01-29 10:39:38 +0800

    linear-regression

--- test.py
+++ test.py
@@ -1,4 +1,31 @@
 import torch
-x = torch.ones(5, 3, dtype=torch.float64)
-print(x)
+from IPython import display
+from matplotlib import pyplot as plt
+import numpy as np
+import random
 
+num_inputs = 2
+num_examples = 1000
+true_w = [2, -3.4]
+true_b = 4.2
+
+
+# randn(m*n) m*n矩阵，标准正态分布
+features = torch.randn(num_examples, num_inputs, dtype=torch.float32)
+
+
+print(features)
+labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
+labels += torch.tensor(np.random.normal(0, 0.01, size=labels.size()), dtype=torch.float32)
+
+
+def use_svg_display():
+    display.set_matplotlib_formats('svg')
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+set_figsize()
+plt.scatter(features[:, 1].numpy(), labels.numpy(), 1)
+
+# 调用show()显示scatter散点图
+plt.show()
